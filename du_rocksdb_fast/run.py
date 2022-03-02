694554bb2eb28674df4fef682e062c5428de1829
Edvard Davtyan <davtyaneg@gmail.com>
2022-03-02 13:47:42 +0100

    Refactor

--- du_rocksdb_fast/run.py
+++ du_rocksdb_fast/run.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 
-import os, sys, subprocess
+import os, sys, subprocess, argparse
 from threading import Thread
 from time import sleep
 from pathlib import Path
 
-
-sst_dump = os.path.dirname(os.path.realpath(__file__)) + '/rocksdb/build/tools/sst_dump'
-print(sst_dump)
-
-def process_files(files, results, tindex):
+def process_files(files, sst_dump, results, tindex):
     print("Got", len(files), "to process")
     results[tindex] = [0] * 60
     for i in range(len(files)):
@@ -21,17 +17,16 @@ def process_files(files, results, tindex):
                 results[tindex][index] += os.path.getsize(filename)
                 print("processed:", str(filename), "index:", index, "progress:", str(round(i / len(files) * 100, 1)) + "%")
 
-if __name__ == "__main__":
-    files = [file for file in Path(sys.argv[1]).glob("*.sst")]
+def main(rocksdb_data, sst_dump, thread_count):
+    files = [file for file in Path(rocksdb_data).glob("*.sst")]
 
-    THREADS = 8
     threads = []
-    results = [None] * THREADS
-    block = (len(files) + THREADS - 1) // THREADS
+    results = [None] * thread_count
+    block = (len(files) + thread_count - 1) // thread_count
     print(len(files))
-    for i in range(THREADS):
+    for i in range(thread_count):
         cfiles = files[block * i : min(block * (i + 1), len(files))]
-        threads.append(Thread(target = process_files, args = (cfiles, results, i, )))
+        threads.append(Thread(target = process_files, args = (cfiles, sst_dump, results, i, )))
         threads[-1].start()
 
     for thread in threads:
@@ -43,3 +38,14 @@ if __name__ == "__main__":
             if results[i]:
                 result[j] += results[i][j]
         print(j, result[j])
+
+if __name__ == "__main__":
+    default_sst_dump = os.path.dirname(os.path.realpath(__file__)) + '/rocksdb/build/tools/sst_dump'
+
+    parser = argparse.ArgumentParser(description='Computes RocksDB column sizes using several threads')
+    parser.add_argument("--rocksdb-data", help="path to rocksdb sst files directory", required=True)
+    parser.add_argument("--sst-dump", help="filename of an existing sst_dump binary", default=default_sst_dump)
+    parser.add_argument("--threads", help="number of threads for processing", default=8)
+    args = parser.parse_args()
+
+    main(args.rocksdb_data, args.sst_dump, int(args.threads))
