a3e2aec5788a4bbd7881dba0e68e18adfec54314
Edvard Davtyan <davtyaneg@gmail.com>
2022-05-19 12:29:22 +0200

    Make ssts_from_strace.py multithreaded

--- ssts_from_strace.py
+++ ssts_from_strace.py
@@ -1,25 +1,57 @@
-import re, subprocess, os.path
+#!/usr/bin/python3
 
-def get_column(file_number):
-    filename = '/home/ubuntu/.near/data/data/' + file_number + '.sst'
-    if not os.path.exists(filename):
-        return "unknown"
-    output = subprocess.check_output(["../scripts/du_rocksdb_fast/rocksdb/build/tools/sst_dump", '--file=' + filename, '--show_properties']).decode('utf-8')
-    for line in output.split('\n'):
-        if "column family name" in line:
-            return line.split(' ')[-1]
-    return "incorrect"
+import os, sys, subprocess, argparse, re
+from threading import Thread
+from time import sleep
+from pathlib import Path
 
-z = dict()
-regex = re.compile("^\d+ (\d{2}:\d{2}:\d{2}).*\/(\d+)\.sst.*$")
-with open("output.txt") as f:
-    for line in f:
-        match = regex.match(line.strip())
-        if match:
-            number = match.groups()[1]
-            column = get_column(number)
-            if not column in z:
-                z[column] = 0
-            z[column] += 1
-print("files opens:")
-print(z)
+def process_files(files, sst_dump, results, tindex):
+    print("Got", len(files), "to process")
+    results[tindex] = [0] * 60
+    for i in range(len(files)):
+        filename = files[i]
+        output = subprocess.check_output([sst_dump, '--file=' + str(filename), '--show_properties']).decode('utf-8')
+        for line in output.split('\n'):
+            if "column family ID" in line:
+                index = int(line.split(' ')[-1]) - 1
+                results[tindex][index] += 1
+                print("processed:", str(filename), "index:", index, "progress:", str(round(i / len(files) * 100, 1)) + "%")
+
+def main(strace_output, sst_dump, thread_count):
+    files = []
+    regex = re.compile("^\d+ (\d{2}:\d{2}:\d{2}).*\"(.*.sst)\".*$")
+    with open(strace_output) as f:
+        for line in f:
+            match = regex.match(line.strip())
+            if match:
+                files.append(match.groups()[1])
+
+    threads = []
+    results = [None] * thread_count
+    block = (len(files) + thread_count - 1) // thread_count
+    print(len(files))
+    for i in range(thread_count):
+        cfiles = files[block * i : min(block * (i + 1), len(files))]
+        threads.append(Thread(target = process_files, args = (cfiles, sst_dump, results, i, )))
+        threads[-1].start()
+
+    for thread in threads:
+        thread.join();
+
+    result = [0] * len(results[0])
+    for j in range(len(result)):
+        for i in range(len(results)):
+            if results[i]:
+                result[j] += results[i][j]
+        print(j, result[j])
+
+if __name__ == "__main__":
+    default_sst_dump = os.path.dirname(os.path.realpath(__file__)) + '/rocksdb/build/tools/sst_dump'
+
+    parser = argparse.ArgumentParser(description='Computes RocksDB column sizes using several threads')
+    parser.add_argument("--strace-output", help="path to strace output", required=True)
+    parser.add_argument("--sst-dump", help="filename of an existing sst_dump binary", default=default_sst_dump)
+    parser.add_argument("--threads", help="number of threads for processing", default=8)
+    args = parser.parse_args()
+
+    main(args.strace_output, args.sst_dump, int(args.threads))
